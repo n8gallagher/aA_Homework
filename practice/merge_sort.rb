f93ae0361538b90647f06342e70cb19ef47e7859
nateg <n8gallagher@gmail.com>
2020-06-18 17:45:30 -0700

    queue and stack adt homework projects added

---
+++ practice/merge_sort.rb
@@ -0,0 +1,19 @@
+
+
+[1, 2, 3, 4, 5]
+
+shuffled_array = [5, 3, 4, 1, 2]
+# split the array in half
+# leftarray
+[5,3]
+[5] [3]
+
+
+
+
+# rightarray
+[4,1,2]
+[4], [1, 2]
+    [1], [2]
+
+output = [3, ]
