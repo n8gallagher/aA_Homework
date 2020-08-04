cebbf8a1035306b1182637f5064c16e0684a4b9e
nateg <n8gallagher@gmail.com>
2020-08-03 20:53:33 -0700

    w10d2 npm homework

--- W9D4/first-ajax-skeleton/js/index.js
+++ W9D4/first-ajax-skeleton/js/index.js
@@ -3,7 +3,7 @@ console.log("Hello from the JavaScript console!");
 // Your AJAX request here
 $.ajax({
     type: 'GET',
-    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=4b002073f68ef429f155fc411c58a0d7",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
     success(data) {
       console.log("We have your weather!")
       console.log(data);
